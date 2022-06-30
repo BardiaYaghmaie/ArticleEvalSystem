47a78597c58cf2e9df69e549370dde7c48ab710c
Bardia <bardia.yaghmaie.81@gmail.com>
2022-06-30 14:32:22 +0430

    Project Done. No Bugs. Latest Version 1401/4/9

--- ArticleEvalSys.cpp
+++ ArticleEvalSys.cpp
@@ -1,4 +1,8 @@
 #include<bits/stdc++.h>
+// #include<iostream>
+// #include<string>
+// #include<vector>
+// #include<algorithm>
 using namespace std;
 
 class User {
@@ -15,8 +19,8 @@ public:
 	void SetUserId(int _user_id) { user_id = _user_id; }
 	int GetUserId() { return user_id; }
 	//I think you forgot adding the function : "GetUsername" and "GetPassword" so i add it below
-	string GetUsername() { return Username; }  
-	string GetPassword() { return Password; }   
+	string GetUsername() { return Username; }
+	string GetPassword() { return Password; }
 	friend class Article;
 	//I added to below line :
 	static int UserCount;
@@ -26,13 +30,13 @@ public:
 vector<User*> Users;
 //int UserCount = 0;
 //instead of this , i use the following code
-int User::UserCount = 0;  
+int User::UserCount = 0;
 class Article {
 
 	int ArticleId;
 	string ArticleTitle;
 	//User Author;
-	User* Author;  
+	User Author;
 	string Content;
 	int ArticleCode;
 	bool Acception;
@@ -42,11 +46,11 @@ public:
 	void SetArticleId(int _articleId) { ArticleId = _articleId; }
 	void SetContent(string _content) { Content = _content; }
 	void SetArticleCode(int _articleCode) { ArticleCode = _articleCode; }
-    string GetArticleContent() { return Content; }
-    string GetArticleTitle() { return ArticleTitle; }
+	string GetArticleContent() { return Content; }
+	string GetArticleTitle() { return ArticleTitle; }
 	// we should have function like : setAuthor and getAuthor
-	User* GetAuthor() { return Author; }  
-	void SetAuthor(User* _author) { Author = _author; }  
+	User GetAuthor() { return Author; }
+	void SetAuthor(User _author) { Author = _author; }
 	// I do not understand why this method is here . Remember to explain this to me tomorrow
 	bool IsArticleValid() {
 
@@ -61,14 +65,14 @@ public:
 //Article Articles[100];
 //vector<Article> Articles;
 // I really like to use pointers because we can use the "NULL" feature of pointers to conditionalize our code better I think
-vector <Article*> Articles;  
+vector <Article*> Articles;
 //int ArticleCount = 0;
-int Article::ArticleCount = 0;  
-User* LoggedInUser = new User();  
+int Article::ArticleCount = 0;
+User LoggedInUser ;
 
 
 void RegisterUser() {
-	cout << "USER REGISTER PAGE" << endl;
+	cout << "\nUSER REGISTERATION PAGE\n" << endl;
 	User* tempUser = new User();
 	cout << "Username: ";
 	string name;
@@ -78,13 +82,15 @@ void RegisterUser() {
 	string pass;
 	cin >> pass;
 	tempUser->SetPassword(pass);
-	cout << "Register Done!" << endl;
+	tempUser->SetUserId(User::UserCount);
+	cout << "\n\tRegister Done!" << endl;
 	//In my idea , i think it is a better idea to use a static field in User class instead of defining a global varible
 	//tempUser.SetUserId(UserCount++);
 	//Users[UserCount] = tempUser;
 	//instead of the above code , we use the following (because of the using of an static field in class "User")
-	User::UserCount++;   
-	Users.push_back(tempUser);  
+	User::UserCount++;
+
+	Users.push_back(tempUser);
 }
 // COMPLETE THIS FUCKIN TASK : ok.consider it done
 
@@ -101,72 +107,84 @@ User* FindUserByUsernameAndPassword(string username, string password) {
 // I think it's necesssary to change the return type of the following function from void to boolean because there might be a possible senario where we can not authorize the user so we should handle this senario and for this we use boolean
 //void LoginUser() {
 bool LoginUser() {
-	cout << "USER LOGIN PAGE" << endl;
+	cout << "\nUSER LOGIN PAGE\n" << endl;
 	string username, password;
 	cout << "Username : ";
 	cin >> username;
 	cout << "Password : ";
 	cin >> password;
-	User* usr = FindUserByUsernameAndPassword(username, password);  
+	User* usr = FindUserByUsernameAndPassword(username, password);
 	if (usr)     // this means that there is at least one user with the given details . so the user can log in
 	{
 		//i should write something here that logs the user in
 		 // so i go up and define a global varibale named : "loggedInUser" which is of type User*;
-		LoggedInUser->SetUserId(usr->GetUserId());  
-		LoggedInUser->SetUsername(usr->GetUsername());  
-		LoggedInUser->SetPassword(usr->GetPassword());  
-		cout << "The user with id : " << LoggedInUser->GetUserId() << " and username : " << LoggedInUser->GetUsername() << " is logged in now !"<< endl;  
-		return true;  
+		LoggedInUser.SetUserId(usr->GetUserId());
+		LoggedInUser.SetUsername(usr->GetUsername());
+		LoggedInUser.SetPassword(usr->GetPassword());
+		//cout << "The user with id : " << LoggedInUser.GetUserId() << " and username : " << LoggedInUser.GetUsername() << " is logged in now !" << endl;
+		cout << endl;
+        return true;
 	}
 	else {
-		cout << "Login failed" << endl;  
-		return false;  
+		cout << "\n\tLogin Failed\n" << endl;
+		return false;
 	}
 
-} 
+}
 
-bool RegisterArticle(){
-	cout << "ARTICLE REGISTER PAGE" << endl;
+void RegisterArticle() {
+	cout << "\nARTICLE REGISTER PAGE\n" << endl;
 	Article* tempArticle = new Article();
-
 	cout << "Title: ";
 	string title;
-	cin >> title;
+	string t;
+	getline(cin, title);
+	getline(cin, title);
 	tempArticle->SetTitle(title);
 	cout << "Article Code: ";
 	int code;
 	cin >> code;
-    cout << "Article content : ";
-    string content;
-    cin >> content;
-    tempArticle->SetContent(content);
+	cout << "Article Content : ";
+	string content, line;
+	while (getline(cin, line)) {
+		content += line + '\n';
+		if (line == ";;;")
+		{
+			content.erase(content.end() - 5, content.end());
+			break;
+		}
+	}
+
+
+
+	tempArticle->SetContent(content);
 	tempArticle->SetArticleCode(code);
-	tempArticle->SetAuthor(LoggedInUser);  
+	tempArticle->SetAuthor(LoggedInUser);
 	//tempArticle.SetArticleId(ArticleCount++);
 	//Articles[ArticleCount] = tempArticle;
-	Article::ArticleCount++;  
-	tempArticle->SetArticleId(Article::ArticleCount);  
+	Article::ArticleCount++;
+	tempArticle->SetArticleId(Article::ArticleCount);
 	if (ValidateArticle(tempArticle)) // check wether this temp article is valid or not . if it is valid , we add this article to the list otherwise we don't add this article to ther article's list
 	{
-		Articles.push_back(tempArticle);  
-		cout << "Register Done!";  
-		return true;  
+		Articles.push_back(tempArticle);
+        cout << "\n\tArticle Registered Successfully\n" << endl;
+
 	}
 	else {
-		cout << "can not register this article";  
+        cout << "\n\tArticle Cannot  Be Register\n" << endl;
 		Article::ArticleCount--; // because we ++ it in above lines but now because we do not regiser , we -- it
-		return false;  
+
 	}
-	
+
 	//tempArticle.Author.ArticleIDs(articleCount++);
 }
 int getEditDistance(std::string first, std::string second)
 {
 	int m = first.length();
 	int n = second.length();
-    
 
-	int T[m + 1][n + 1];
+
+	vector< vector<int> > T(m + 1, vector<int>(n + 1, 0));
 	for (int i = 1; i <= m; i++) {
 		T[i][0] = i;
 	}
@@ -177,86 +195,221 @@ int getEditDistance(std::string first, std::string second)
 
 	for (int i = 1; i <= m; i++) {
 		for (int j = 1; j <= n; j++) {
-			int weight = first[i - 1] == second[j - 1] ? 0: 1;
-			T[i][j] = std::min(std::min(T[i-1][j] + 1, T[i][j-1] + 1), T[i-1][j-1] + weight);
+			int weight = first[i - 1] == second[j - 1] ? 0 : 1;
+			T[i][j] = std::min(std::min(T[i - 1][j] + 1, T[i][j - 1] + 1), T[i - 1][j - 1] + weight);
 		}
 	}
 
 	return T[m][n];
 }
-double Similarity(string first, string second){
-double max_length = std::max(first.length(), second.length());
+double Similarity(string first, string second) {
+	double max_length = std::max(first.length(), second.length());
 	if (max_length > 0) {
 		return (max_length - getEditDistance(first, second)) / max_length;
 	}
 	return 1.0;
 
 }
-int WordCounter(Article* article){
-    int words = 0;
-  
-  int lenOfSentence = article->GetArticleContent().length();
+//int WordCounter(Article* article) {
+int WordCounter(string text) {
+	int words = 0;
 
-  // run for loop from i = 0 to i = lenOfSentence
-  // to iterate through each character of the string
-  for(int i = 0; i < lenOfSentence; i++)
-  {
-    // check if current character is a space
-    if(article->GetArticleContent()[i] == ' ')
-    {
-      // if it is a space, increment word count
-      words++;
-    }
+	int lenOfSentence = text.length();
 
-  }
-  // at the end of the for loop, no. of spaces have been
-  // counted. Increment word count one more time to get
-  // no. of words
-  words = words + 1;
-  return words;
-   
+	// run for loop from i = 0 to i = lenOfSentence
+	// to iterate through each character of the string
+	for (int i = 0; i < lenOfSentence; i++)
+	{
+		// check if current character is a space
+		if (text[i] == ' ')
+		{
+			// if it is a space, increment word count
+			words++;
+		}
+
+	}
+	// at the end of the for loop, no. of spaces have been
+	// counted. Increment word count one more time to get
+	// no. of words
+	words = words + 1;
+	return words;
+
 }
+template<typename T>
+bool IsxInArray(vector<T> arr, int x) {
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (x == arr[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+//regex TO DO
+//bool SpellChecker(string text) {
+//	//check ; , :  ? !  - spells : 
+//
+//	bool flag1 = true;
+//	for (int i = 0; i < text.length() - 1; i++)
+//	{
+//		if (
+//			(text[i] == ';' && text[i + 1] == ';') ||
+//			(text[i] == ':' && text[i + 1] == ':') ||
+//			(text[i] == ',' && text[i + 1] == ',') ||
+//			(text[i] == '?' && text[i + 1] == '?') ||
+//			(text[i] == '!' && text[i + 1] == '!')
+//			)
+//		{
+//			flag1 = false;
+//		}
+//	}
+//	//check that there is capital letter after any dot and also at the first char of the text
+//	bool flag2 = true;
+//	bool flag2_ = isupper(text[1]);
+//
+//	size_t g = text.find(".");
+//	while (g != string::npos)
+//	{
+//		//if (isupper(text[g + 1]) == false)
+//		//{
+//			//flag2 = false;
+//			//break;
+//		//}
+//		char c = text[g + 1];
+//		if (isalpha(c) == true)
+//		{
+//			if (isupper(c) == false)
+//			{
+//				flag2 = false;
+//				break;
+//			}
+//			else {
+//				text.erase(text.begin() + g);
+//				g = text.find(".");
+//			}
+//		}
+//		else {
+//			text.erase(text.begin() + g);
+//			g = text.find(".");
+//		}
+//	}
+//	//check that there is an space after the . , ; : ( ) ? ! 
+//	bool flag3 = true;
+//	vector<char> v = { '.',';',':',')','(','?','!' };
+//	for (int i = 0; i < text.length(); i++)
+//	{
+//		char x = text[i];
+//		if (IsxInArray(v, x))
+//		{
+//			if (text[i + 1] == ' ' ||
+//				(text[i + 1] == text.length() && text[text.length() - 1] == '.')
+//				)
+//			{
+//				// do nothing
+//			}
+//			else {
+//				flag3 = false;
+//				break;
+//			}
+//		}
+//	}
+//	if (flag1 && flag2 && flag2_ && flag3)
+//	{
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//
+//}
 bool ValidateArticle(Article* article) {
-    for (int i = 0; i < Articles.size(); i++) {
-        if (Articles[i] != article) {
-            if (Similarity(article->GetArticleContent(), Articles[i]->GetArticleContent()) > 0.5)
-                return false;
+	//article content validation first
+	bool flagArticleContent = true;
+	if (Article::ArticleCount == 1)
+	{
+		//simply do nothing
+		//return true;
 
-        }
-    }
-    if (WordCounter(article) > 5000 || WordCounter(article) < 100)
-        return false;
-    
-    // TITLE VALIDATION
-    return true;
+	}
+	else {
+		for (int i = 0; i < Articles.size(); i++) {
+			if (Articles[i] != article) {
+				if (Similarity(article->GetArticleContent(), Articles[i]->GetArticleContent()) > 0.5)
+					flagArticleContent = false;
+				//return false;
 
-    
-	
+			}
+		}
+	}
+	if (WordCounter(article->GetArticleContent()) > 5000 || WordCounter(article->GetArticleContent()) < 100)
+		flagArticleContent = false;
+	//return false;
 
+	// according to the project assignment  , the template should be at least 3 paragraphs
+	bool flagParagraphCountCondition = true;
+	string content = article->GetArticleContent();
+	int counter = 0;
+	size_t index_of_new_line = content.find("\n");
+	while (index_of_new_line != string::npos)
+	{
+		counter++;
+		content.erase(content.begin() + index_of_new_line);
+		index_of_new_line = content.find("\n");
+	}
+	counter--;
+	if (counter < 3)
+	{
+		flagParagraphCountCondition = false;
+	}
+	// TITLE VALIDATION
+	bool flagArticleTitle = true;
+	if (WordCounter(article->GetArticleTitle()) > 12)
+	{
+		flagArticleTitle = false;
+	}
+	// spell checking . just calling the function here : 
+	//bool flagSpellChecker = SpellChecker(article->GetArticleContent());
+	//total validation : 
+	if (flagArticleContent && flagArticleTitle && flagParagraphCountCondition /*&& flagSpellChecker*/)
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+ 
 }
 
-void ShowAllArticles(){
-    for (int i = 0; i <  Articles.size(); i++)
-    {
-        cout << Articles[i]->GetArticleTitle() << "By: " << Articles[i]->GetAuthor() << endl;
-        /* code */
+void ShowAllArticles() {
+    if (Articles.size() == 0)
+        cout << "\n\t404 - No Articles Available\n"<< endl;
+    else {
+	for (int i = 0; i < Articles.size(); i++)
+	{
+		cout << "\n" << Articles[i]->GetArticleTitle() << " By: " << Articles[i]->GetAuthor().GetUsername() << "\n" <<endl;
+		/* code */
+	}
     }
 }
+
+
 
 int main() {
-	cout << "WELCOME TO ARTICLE EVAL SYSTEM" << endl;
 firstpage:
-	cout << "1. Sign Up\t2. Login" << endl;
+	cout << "\nWELCOME TO ARTICLE EVAL SYSTEM\n" << endl;
+	cout << "\t1. Sign Up\t2. Login" << endl;
 	int command;
 	cin >> command;
 	switch (command) {
 	case 1:
 		RegisterUser();
-		//goto login;
+		goto login;
 
-	case 2:{
-		bool flag = LoginUser();  
-		if (flag)  
+	case 2: {
+	login:
+		bool flag = LoginUser();
+		if (flag)
 		{
 			goto panel;
 
@@ -264,36 +417,39 @@ firstpage:
 		else {
 			goto firstpage;
 		}
+
 	}
 	default:
 		cout << "Error: Unknown command!!";
 		goto firstpage;
+
 	}
 panel:
-    cout << " Welcome " << LoggedInUser->GetUsername()<< " :)" << endl;
-	cout << "User Panel" << endl;
-    cout << "1. Register Article\t2. See List of Articles\t3. Sign Out" << endl;
-    int cmd;
-    cin >> cmd;
-    switch (cmd) {
-        case 1:{
+	cout << "\n\tWelcome " << LoggedInUser.GetUsername() << " :)\n" << endl;
+panel2:
+	cout << "User Panel\n" << endl;
+	cout << "\t1. Register Article        2. See List of Articles        3. Sign Out" << endl;
+	int cmd;
+	cin >> cmd;
+	switch (cmd) {
+	case 1: {
 
-            bool flg = RegisterArticle();
-            if(flg){
-                cout << "register done";
-            }
+		RegisterArticle();
+		goto panel2;
 
-            else{
-                cout << "????";
-            }
-        }
-        case 2:
-            ShowAllArticles();
-        case 3:
-            LoggedInUser = NULL;
-            goto firstpage;
-        default:
-            goto panel;
-    }
+	}
+	case 2:
+		ShowAllArticles();
+		goto panel2;
+	case 3: {
+		LoggedInUser.SetPassword("");
+		LoggedInUser.SetUserId(-1);
+		LoggedInUser.SetUsername("");
+		goto firstpage;
+	}
+
+	default:
+		goto panel;
+	}
 
 }
